992f149a86467d12af71ad8e41606d5b48ebbc1d
schwienernitzel <pfelix0803@gmail.com>
2025-05-17 00:42:11 +0200

    mozart: bluetooth: Configure generic `bdroid_buildcfg.h`
    Signed-off-by: schwienernitzel <pfelix0803@gmail.com>

---
+++ bluetooth/bdroid_buildcfg.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2025 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _BDROID_BUILDCFG_H
+#define _BDROID_BUILDCFG_H
+
+#define BTM_DEF_LOCAL_NAME   "MediaPad M2 8.0"
+
+#endif
