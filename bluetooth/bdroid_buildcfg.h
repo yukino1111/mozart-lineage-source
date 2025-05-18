2a8d984582f55d290e4a5a8d107151193e6533bc
R0rt1z2 <me@r0rt1z2.com>
2025-05-18 23:55:51 +0200

    mozart: bluetooth: Load `BTM_DEF_LOCAL_NAME` dynamically
    Signed-off-by: schwienernitzel <pfelix0803@gmail.com>

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -7,7 +7,36 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "MediaPad M2 8.0"
+#include <stdint.h>
+#include <string.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+int property_get(const char *key, char *value, const char *default_value);
+#ifdef __cplusplus
+}
+#endif
+
+inline const char* BtmGetDefaultName()
+{
+	char device[92];
+	property_get("ro.product.model", device, "");
+
+        if (strstr(device, "M2-801W") != NULL) {
+            return "M2-801W";
+        } else if (strstr(device, "M2-801L") != NULL) {
+            return "M2-801L";
+        } else if (strstr(device, "M2-802L") != NULL) {
+            return "M2-802L";
+        } else if (strstr(device, "M2-803L") != NULL) {
+            return "M2-803L";
+        }
+
+	return "MediaPad M2 8.0";
+}
+
+#define BTM_DEF_LOCAL_NAME   BtmGetDefaultName()
 #define BTM_BYPASS_EXTRA_ACL_SETUP TRUE
 
 #endif
