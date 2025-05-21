045891dfcd673c6437f6fa130e9da023c915d75c
schwienernitzel <pfelix0803@gmail.com>
2025-05-21 18:27:33 +0200

    mozart: Configure GNSS legacy interface
    Signed-off-by: schwienernitzel <pfelix0803@gmail.com>

--- device.mk
+++ device.mk
@@ -62,6 +62,10 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl
 
+# GPS
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@1.0-impl.legacy
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.0-impl \
