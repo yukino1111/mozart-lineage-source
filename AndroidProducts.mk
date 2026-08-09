544d76f81eb44ea863d8d16120f9c752e75d5bd6
yukino1111 <107763725+yukino1111@users.noreply.github.com>
2026-08-09 14:40:00 +0800

    mozart-patches: publish maintained LineageOS 18.1 source
    Continue from the complete kirin930-dev device-tree history at
    4a057c26bbc1b85782d10fb99907ec14974433a1; this commit contains only the maintained Android 11 device and patch-stack changes.

--- AndroidProducts.mk
+++
@@ -1,8 +0,0 @@
-#
-# Copyright (C) 2025 The LineageOS Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_mozart.mk
--- vendorsetup.sh
+++
@@ -1,7 +0,0 @@
-#
-# Copyright (C) 2025 The LineageOS Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-add_lunch_combo lineage_mozart-userdebug
